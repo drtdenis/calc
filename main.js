4aebccc58d1936a26aa657886ab9fa3f5b5c87dc
drtdenis <drouetdenis2@gmail.com>
2021-03-12 14:47:50 +0100

    calc

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const operation = require('./operations')
+/*const operation = require('./operations')
 
 
 
@@ -6,7 +6,7 @@ console.log(operation.add(10, 11))
 console.log(operation.sub(10, 5))
 console.log(operation.mul(2, 10))
 console.log(operation.div(5, 3))
-console.log(operation.modulo(10, 2))
+console.log(operation.modulo(10, 2))*/
 
 
 
@@ -14,3 +14,6 @@ console.log(operation.modulo(10, 2))
 
 //console.log('Hello, World!!
 //')
+
+const {calc} = require('./calc')
+console.log(calc('*', 10, 10))
